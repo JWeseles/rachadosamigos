11d529546d3e5ed3fb578583fc712c9956596a67
Alves <jweseles_al@hotmail.com>
2024-01-01 12:48:27 -0300

    deletado runtime, criação de gitignore

--- rachadosamigos/settings.py
+++ rachadosamigos/settings.py
@@ -85,14 +85,14 @@ WSGI_APPLICATION = 'rachadosamigos.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-"""""
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-"""""
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
